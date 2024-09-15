4b550cc5d901cc013cf2a71179ea4859c70fe314
dicenull <dicenull.dev@gmail.com>
2024-09-15 22:22:13 +0900

    dntの準備

---
+++ scripts/build_npm.ts
@@ -0,0 +1,29 @@
+// ex. scripts/build_npm.ts
+import { build, emptyDir } from "@deno/dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  shims: {
+    deno: true,
+  },
+  package: {
+    name: "hanabijs",
+    version: Deno.args[0],
+    description: "Draw fireworks package.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/dicenull/hanabijs.git",
+    },
+    bugs: {
+      url: "https://github.com/dicenull/hanabijs/issues",
+    },
+  },
+  postBuild() {
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});

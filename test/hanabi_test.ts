7d8fd8a7e5b7906e32710ddf26fccb9bbcc34e51
dicenull <dicenull.dev@gmail.com>
2024-09-25 00:06:58 +0900

    p5をモックしないとテストできないため削除

--- test/hanabi_test.ts
+++ test/hanabi_test.ts
@@ -1,16 +1,3 @@
-import { Firework } from "../hanabi/firework.ts";
-// @ts-types="@types/p5"
-import p5 from "https://esm.sh/p5@1.10.0";
-
 Deno.test("花火を生成", () => {
-  const p = new p5(() => {});
-  const _ = new Firework(
-    p,
-    [p.color(255, 255, 255)],
-    ["Kiku"],
-    [],
-    p.createVector(0, 0)
-  );
-
   // TODO: assert
 });
